6dc8e13445747c75233ce2c11822b14af9e0b315
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2025-09-19 14:10:34 +0000

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
 
 import { motion } from "framer-motion";
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card } from "./ui/card";
+import { Button } from "./ui/button";
 import { Github, ExternalLink } from "lucide-react";
 
 const projects = [
